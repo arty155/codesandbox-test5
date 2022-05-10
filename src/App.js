aaa188e46f04b67e4b41f61ac2a4fdc77421fa14
arty155 <104909264+arty155@users.noreply.github.com>
2022-05-10 21:37:45 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { PrymaryButton } from "./components/atoms/button/PrimaryButton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      aaaaaaaa
+      <br />
+      <PrymaryButton>aaa</PrymaryButton>
+    </div>
+  );
+}
