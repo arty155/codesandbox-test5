2d485e8d49edf1a3acdd545576dd2f5b53f19124
arty155 <104909264+arty155@users.noreply.github.com>
2022-05-10 22:14:57 +0900

    moleculeまで

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { PrymaryButton } from "./components/atoms/button/PrimaryButton";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      aaaaaaaa
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
       <br />
-      <PrymaryButton>aaa</PrymaryButton>
+      <SearchInput />
     </div>
   );
 }
