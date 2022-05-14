5ddd93cd93b6234bd29e9c086641585ab09bbbe2
arty155 <104909264+arty155@users.noreply.github.com>
2022-05-14 10:10:28 +0900

    test

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,15 @@
+import React from "react";
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
