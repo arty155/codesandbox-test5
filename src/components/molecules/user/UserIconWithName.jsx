a809554a2291873a8f8c206b0787e5ffae942813
arty155 <104909264+arty155@users.noreply.github.com>
2022-05-14 22:59:43 +0900

    test

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -4,14 +4,14 @@ import { UserContext } from "../../../providers/UserProvider";
 
 export const UserIconWithName = (props) => {
   const { image, name } = props;
-  const context = useContext(UserContext);
-  console.log(context);
-  console.log(context.userinfo.isAdmin);
+  const { userInfo } = useContext(UserContext);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
 
   return (
     <SContainer>
       <SImg height={160} width={160} alt="プロフィール写真" src={image} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
 };
@@ -29,4 +29,10 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`;
+
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
