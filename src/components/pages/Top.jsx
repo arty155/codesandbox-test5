cd35cc45d68108c71af74dea1251829ad9185b74
arty155 <104909264+arty155@users.noreply.github.com>
2022-05-14 10:13:53 +0900

    link

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const onClickAdmin = () => alert("管理");
   return (
     <SContainer>
       <h2>TOPページです</h2>
-      <SecondaryButton>管理者ユーザー</SecondaryButton>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
       <br />
       <br />
       <SecondaryButton>一般ユーザー</SecondaryButton>
